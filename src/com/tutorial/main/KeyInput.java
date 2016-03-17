e71e0f227636c4847685a2b4ed18dbf6d7dc5ccf
Dave Carrell <dave.carrell@gmail.com>
2016-03-16 22:34:57 -0500

    Fixed keyboard input lag, but still bugs on holding multiple keys
    hold left, hold up and right, let go of up and right and you're not 
    traveling left. For some reason the right direction is remembered last

--- src/com/tutorial/main/KeyInput.java
+++ src/com/tutorial/main/KeyInput.java
@@ -6,9 +6,15 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter {
 	
 	private Handler handler;
+	private boolean[] keyDown = new boolean[4];
 	
 	public KeyInput(Handler handler) {
 		this.handler = handler;
+		
+		keyDown[0] = false;
+		keyDown[1] = false;
+		keyDown[2] = false;
+		keyDown[3] = false;
 	}
 	
 	public void keyPressed(KeyEvent e) {
@@ -18,10 +24,10 @@ public class KeyInput extends KeyAdapter {
 			if (o.getId() == ID.PLAYER) {
 				// Key events for player 1
 				
-				if (key == KeyEvent.VK_W) o.setVelY(-5);
-				if (key == KeyEvent.VK_S) o.setVelY(5);
-				if (key == KeyEvent.VK_D) o.setVelX(5);
-				if (key == KeyEvent.VK_A) o.setVelX(-5);
+				if (key == KeyEvent.VK_W) { o.setVelY(-5); keyDown[0] = true; }
+				if (key == KeyEvent.VK_S) { o.setVelY(5); keyDown[1] = true; }
+				if (key == KeyEvent.VK_D) { o.setVelX(5); keyDown[2] = true; }
+				if (key == KeyEvent.VK_A) { o.setVelX(-5); keyDown[3] = true; }
 			}
 		}
 		
@@ -36,10 +42,15 @@ public class KeyInput extends KeyAdapter {
 			if (o.getId() == ID.PLAYER) {
 				// Key events for player 1
 				
-				if (key == KeyEvent.VK_W) o.setVelY(0);
-				if (key == KeyEvent.VK_S) o.setVelY(0);
-				if (key == KeyEvent.VK_D) o.setVelX(0);
-				if (key == KeyEvent.VK_A) o.setVelX(0);
+				if (key == KeyEvent.VK_W) keyDown[0] = false; //o.setVelY(0);
+				if (key == KeyEvent.VK_S) keyDown[1] = false; //o.setVelY(0);
+				if (key == KeyEvent.VK_D) keyDown[2] = false; //o.setVelX(0);
+				if (key == KeyEvent.VK_A) keyDown[3] = false; //o.setVelX(0);
+				
+				// Vertical movement
+				if (!keyDown[0] && !keyDown[1]) o.setVelY(0);
+				// Horizontal movement
+				if (!keyDown[2] && !keyDown[3]) o.setVelX(0);
 			}
 		}
 	}
